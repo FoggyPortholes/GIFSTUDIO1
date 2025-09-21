d23672bfa6a709587212c148c3de1ee1a6e56314
T B <97633701+FoggyPortholes@users.noreply.github.com>
2025-09-21 11:31:21 -0700

    Rebuild Gif Studio interface around core GIF tools

--- src/types/gifenc.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'gifenc' {
-  export function GIFEncoder(): any;
-  export function quantize(data: Uint8ClampedArray | Uint8Array, maxColors: number): Uint8Array;
-  export function applyPalette(data: Uint8ClampedArray | Uint8Array, palette: Uint8Array): Uint8Array;
-}
