d95dbc986988a752910a1d775c12ef48a2ad0179
T B <97633701+FoggyPortholes@users.noreply.github.com>
2025-09-21 17:57:33 -0700

    Prune obsolete gif tooling

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envPrefix: ['VITE_', 'OFFLINE_'],
   test: {
-    setupFiles: ['tests/setup.ts'],
     environment: 'happy-dom',
     globals: true,
   },
